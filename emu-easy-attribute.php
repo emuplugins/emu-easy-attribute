4174bb351162eb982eb2668097e0667d16ae3ece
Emu Plugins <147978343+emuplugins@users.noreply.github.com>
2025-02-14 07:45:39 -0300

    Update emu-easy-attribute.php

--- emu-easy-attribute.php
+++ emu-easy-attribute.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Emu Easy Attribute
  * Description: Plugin para atribuição de ativos gratuitos
- * Version: 1.0.9
+ * Version: 1.1.0
  * Author: Emu Plugins
  */
 
@@ -10,13 +10,14 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-// Obtendo o nome do plugin e removendo o sufixo '-main'
-$plugin_slug = rtrim(plugin_dir_path(__FILE__), '/') ;
-$plugin_slug = basename($plugin_slug);
+
+// Caminho absoluto do arquivo principal do plugin
+
+$plugin_slug = basename(__DIR__);  // Diretório do plugin
 if (substr($plugin_slug, -5) === '-main') {
     $plugin_slug = substr($plugin_slug, 0, -5); // Remove o sufixo '-main'
 }
-
+$plugin_dir = basename(__DIR__); // Mantemos o diretório original para referência
 require_once plugin_dir_path(__FILE__) . 'update-handler.php';
 require_once plugin_dir_path(__FILE__) . 'includes/post-type.php';
 require_once plugin_dir_path(__FILE__) . 'includes/meta-boxes.php';
