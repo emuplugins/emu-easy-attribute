edd10795aae40f989e52e43dd43e9552f8075e0a
Emu Plugins <147978343+emuplugins@users.noreply.github.com>
2025-02-14 02:38:16 -0300

    Update update-handler.php

--- update-handler.php
+++ update-handler.php
@@ -1,14 +1,24 @@
 <?php
-class Emu_Easy_Attribute_Updater {
-    private $api_url = 'https://raw.githubusercontent.com/emuplugins/emu-easy-attribute/refs/heads/main/info.json';
 
-    public function __construct() {
+if (!defined('ABSPATH')) {
+    exit;
+}
+if ( ! class_exists( 'Emu_Updater' ) ) {
+    
+class Emu_Updater {
+    private $plugin_slug;
+    private $api_url;
+
+    public function __construct($plugin_slug) {
+        $this->plugin_slug = $plugin_slug;
+        $this->api_url = 'https://raw.githubusercontent.com/emuplugins/' . $this->plugin_slug . '/refs/heads/main/info.json';
+
         add_filter('plugins_api', [$this, 'plugin_info'], 20, 3);
         add_filter('site_transient_update_plugins', [$this, 'check_for_update']);
     }
 
     public function plugin_info($res, $action, $args) {
-        if ($action !== 'plugin_information' || $args->slug !== 'emu-easy-attribute') {
+        if ($action !== 'plugin_information' || $args->slug !== $this->plugin_slug) {
             return $res;
         }
 
@@ -50,11 +60,11 @@ class Emu_Easy_Attribute_Updater {
             return $transient;
         }
 
-        $current_version = get_plugin_data(__DIR__ . '/emu-easy-attribute.php')['Version'];
+        $current_version = get_plugin_data(WP_PLUGIN_DIR . '/' . $this->plugin_slug . '/' . $this->plugin_slug . '.php')['Version'];
         if (version_compare($current_version, $plugin_info->version, '<')) {
-            $transient->response['emu-easy-attribute/emu-easy-attribute.php'] = (object) [
+            $transient->response[$this->plugin_slug . '/' . $this->plugin_slug . '.php'] = (object) [
                 'slug'        => $plugin_info->slug,
-                'plugin'      => 'emu-easy-attribute/emu-easy-attribute.php',
+                'plugin'      => $this->plugin_slug . '/' . $this->plugin_slug . '.php',
                 'new_version' => $plugin_info->version,
                 'package'     => $plugin_info->download_url,
                 'tested'      => $plugin_info->tested,
@@ -64,10 +74,11 @@ class Emu_Easy_Attribute_Updater {
         return $transient;
     }
 }
+}
 
-new Emu_Easy_Attribute_Updater();
+new Emu_Updater($plugin_slug);
 
-add_filter('plugin_action_links_emu-easy-attribute/emu-easy-attribute.php', function($actions) {
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), function($actions) {
     $slug = basename(__DIR__);
     $url = wp_nonce_url(admin_url("plugins.php?force-check-update=$slug"), "force_check_update_$slug");
     $actions['check_update'] = '<a href="' . esc_url($url) . '">Check for Update</a>';
@@ -93,6 +104,10 @@ add_action('admin_notices', function() {
 
 add_filter('upgrader_post_install', function($response, $hook_extra, $result) {
     global $wp_filesystem;
+
+    if (!function_exists('get_filesystem_method')) {
+        require_once(ABSPATH . 'wp-admin/includes/file.php');
+    }
 
     $current_plugin_slug = basename(__DIR__);
     $proper_destination = WP_PLUGIN_DIR . '/' . $current_plugin_slug;
@@ -123,5 +138,3 @@ if (!function_exists('auto_reactivate_plugin_after_update')) {
     }
     add_action('upgrader_process_complete', 'auto_reactivate_plugin_after_update', 10, 2);
 }
-
-
