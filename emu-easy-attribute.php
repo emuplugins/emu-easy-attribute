713417f124787a56ec926d0bd9c4b6368ed1b489
emuplugins <tonnysantanasantos@gmail.com>
2025-02-16 13:12:16 -0300

    TESTE

--- emu-easy-attribute.php
+++ emu-easy-attribute.php
@@ -12,12 +12,28 @@ require_once plugin_dir_path(__FILE__) . 'includes/option-page/core.php';
 
 // Sistema de atualização do plugin
 
-$plugin_slug = basename(__DIR__);
-if (substr($plugin_slug, -5) === '-main') {
-    $plugin_slug = substr($plugin_slug, 0, -5);
-}
-$self_plugin_dir = basename(__DIR__);
+// Inicialização otimizada (fora da classe)
+add_action('admin_init', function() {
+    // Carrega apenas no admin e verifica necessidade
+    if (!current_user_can('update_plugins')) return;
 
-require_once plugin_dir_path(__FILE__) . 'update-handler.php';
+    $plugin_slug = basename(__DIR__);
+    if (substr($plugin_slug, -5) === '-main') {
+        $plugin_slug = substr($plugin_slug, 0, -5);
+    }
+    $self_plugin_dir = basename(__DIR__);
 
-new Emu_Updater($plugin_slug, $self_plugin_dir);
+    require_once plugin_dir_path(__FILE__) . 'update-handler.php';
+
+    // Self Update
+    new Emu_Updater($plugin_slug, $self_plugin_dir);
+    
+// Força verificação de atualizações
+add_action('admin_init', function() {
+    wp_update_plugins();
+});
+// Atualizações de terceiros
+add_action('load-update-core.php', 'emu_load_plugin_updates', 6);
+add_action('load-plugins.php', 'emu_load_plugin_updates', 6);
+
+});
