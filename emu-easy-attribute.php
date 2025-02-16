c5d1241ae81c7f5f0c0878b5feaf1687cbd0224c
emuplugins <tonnysantanasantos@gmail.com>
2025-02-16 12:35:18 -0300

    aaaa

--- emu-easy-attribute.php
+++ emu-easy-attribute.php
@@ -12,37 +12,13 @@ require_once plugin_dir_path(__FILE__) . 'includes/option-page/core.php';
 
 // Sistema de atualização do plugin
 
-// Verifica se o plugin principal está ativo
-if ( ! function_exists( 'is_plugin_active' ) ) {
-    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+$plugin_slug = basename(__DIR__);
+if (substr($plugin_slug, -5) === '-main') {
+    $plugin_slug = substr($plugin_slug, 0, -5);
 }
-if (!is_plugin_active('emu-update-core/emu-update-core.php')) {
+$self_plugin_dir = basename(__DIR__);
 
-    if (!defined('EMU_UPDATE_HANDLER')) {
-        define('EMU_UPDATE_HANDLER', __FILE__); // Se o principal não estiver ativo, este assume
-    }
+require_once plugin_dir_path(__FILE__) . 'update-handler.php';
 
-    function emu_load_on_update_pages() {
-        global $pagenow;
-
-        $update_pages = ['update-core.php', 'update.php', 'plugins.php', 'themes.php'];
-
-        if (in_array($pagenow, $update_pages)) {
-
-            $plugin_slug = basename(__DIR__);
-            if (substr($plugin_slug, -5) === '-main') {
-                $plugin_slug = substr($plugin_slug, 0, -5);
-            }
-            $self_plugin_dir = basename(__DIR__);
-
-            require_once plugin_dir_path(__FILE__) . 'update-handler.php';
-
-            new Emu_Updater($plugin_slug, $self_plugin_dir);
-            
-            add_action('upgrader_process_complete', 'emu_handle_plugin_update', 10, 2);
-        }
-    }
-
-    add_action('admin_init', 'emu_load_on_update_pages');
-}
-
+new Emu_Updater($plugin_slug, $self_plugin_dir);
+add_action('upgrader_process_complete', 'emu_handle_plugin_update', 10, 2);
