0bb04ef67057b02db0a8460b2e92549826a2c307
emuplugins <tonnysantanasantos@gmail.com>
2025-02-16 11:47:04 -0300

    a

--- emu-easy-attribute.php
+++ emu-easy-attribute.php
@@ -6,31 +6,43 @@
  * Author: Emu Plugins
  */
 
- if (!defined('ABSPATH')) exit;
-
-// Carrega funções de atualização apenas na página de atualizações do wordpress
-
-function emu_load_on_update_pages() {
-    global $pagenow;
-
-    $update_pages = ['update-core.php', 'update.php', 'plugins.php', 'themes.php'];
-
-    if (in_array($pagenow, $update_pages)) {
-
-        require_once plugin_dir_path(__FILE__) . 'update-handler.php';
-        // Configuração do auto-update para o próprio plugin
-        $plugin_slug = basename(__DIR__);
-        if (substr($plugin_slug, -5) === '-main') {
-            $plugin_slug = substr($plugin_slug, 0, -5);
-        }
-        $self_plugin_dir = basename(__DIR__);
-
-        new Emu_Updater($plugin_slug, $self_plugin_dir);
-        add_action('upgrader_process_complete', 'emu_handle_plugin_update', 10, 2);
-
-    }
-}
-
 require_once plugin_dir_path(__FILE__) . 'includes/post-type.php';
 require_once plugin_dir_path(__FILE__) . 'includes/meta-boxes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/option-page/core.php';
+
+// Sistema de atualização do plugin
+
+// Verifica se o plugin principal está ativo
+if ( ! function_exists( 'is_plugin_active' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+if (!is_plugin_active('emu-update-core/emu-update-core.php')) {
+
+    if (!defined('EMU_UPDATE_HANDLER')) {
+        define('EMU_UPDATE_HANDLER', __FILE__); // Se o principal não estiver ativo, este assume
+    }
+
+    function emu_load_on_update_pages() {
+        global $pagenow;
+
+        $update_pages = ['update-core.php', 'update.php', 'plugins.php', 'themes.php'];
+
+        if (in_array($pagenow, $update_pages)) {
+
+            $plugin_slug = basename(__DIR__);
+            if (substr($plugin_slug, -5) === '-main') {
+                $plugin_slug = substr($plugin_slug, 0, -5);
+            }
+            $self_plugin_dir = basename(__DIR__);
+
+            require_once plugin_dir_path(__FILE__) . 'update-handler.php';
+
+            new Emu_Updater($plugin_slug, $self_plugin_dir);
+            
+            add_action('upgrader_process_complete', 'emu_handle_plugin_update', 10, 2);
+        }
+    }
+
+    add_action('admin_init', 'emu_load_on_update_pages');
+}
+
