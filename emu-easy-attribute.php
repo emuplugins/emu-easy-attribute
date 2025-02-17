77c8d8694c31818e99c03af79531a48d0c69dc8a
emuplugins <tonnysantanasantos@gmail.com>
2025-02-16 23:25:59 -0300

    variaveis movidas para o topo

--- emu-easy-attribute.php
+++ emu-easy-attribute.php
@@ -5,9 +5,13 @@
  * Version: 1.1.2
  * Author: Emu Plugins
  */
-if(!is_defined('ABSPATH')){
-    exit;
-}
+ if (!defined('ABSPATH')) exit;
+
+ $plugin_slug = basename(__DIR__);
+ if (substr($plugin_slug, -5) === '-main') {
+     $plugin_slug = substr($plugin_slug, 0, -5);
+ }
+ $self_plugin_dir = basename(__DIR__);
 
 // Impedir qualquer tentativa de carregar traduções aqui
 
@@ -30,12 +34,6 @@ require_once plugin_dir_path(__FILE__) . 'includes/meta-boxes.php';
 require_once plugin_dir_path(__FILE__) . 'includes/option-page/core.php';
 
 // Sistema de atualização do plugin
-
-$plugin_slug = basename(__DIR__);
-if (substr($plugin_slug, -5) === '-main') {
-    $plugin_slug = substr($plugin_slug, 0, -5);
-}
-$self_plugin_dir = basename(__DIR__);
 
 require_once plugin_dir_path(__FILE__) . 'update-handler.php';
 
